5d44df7c9fcfa332f453e17787aa14b75a736350
Nuraly <ab.nu2017@mail.ru>
2025-03-31 00:29:39 +0500

    1

--- src/main/java/com/example/ads/HelloApplication.java
+++ src/main/java/com/example/ads/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication {
 //            int inp = input.nextInt();
 //            arr[i] = inp;
 //        }
+        // Commented function calls for testing
         //System.out.println(problem1(n, min));
         //System.out.println(problem2(n));
         //System.out.println(problem3(n));
@@ -22,10 +23,10 @@ public class HelloApplication {
         //problem7(n-1, arr);
         //System.out.println(problem8(s) ? "Yes" : "No");
         //System.out.println(problem9(a, n));
-        System.out.println(problem10(a,n));
+        //System.out.println(problem10(a,n));
 
     }
-    public static int problem1(int n, int min) {
+    public static int problem1(int n, int min) { //O(n) An array of size n is input.The n numbers are read and the minimum among them is found.The found minimum is returned.
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             int inp = input.nextInt();
@@ -36,7 +37,7 @@ public class HelloApplication {
         }
         return min;
     }
-    public static double problem2(int n) {
+    public static double problem2(int n) { //O(n) An array of size n is entered. The numbers are summed up, then their average value is calculated.
         double avg = 0;
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
@@ -47,7 +48,7 @@ public class HelloApplication {
         avg/=n;
         return avg;
     }
-    public static String problem3(int n) {
+    public static String problem3(int n) {  //O(1) Checking if the num is prime or composite. If it is divided by int from 2 to 9, its prime.
         if (n==1 || n==2 || n==3 || n==5 || n==7) {
             return "Prime";
         }
@@ -60,13 +61,13 @@ public class HelloApplication {
             return "Prime";
         }
     }
-    public static int problem4(int n) {
+    public static int problem4(int n) {  //O(n) It finds the factorial of n
         if (n == 1){
             return 1;
         }
         return n*problem4(n-1);
     }
-    public static int problem5(int n) {
+    public static int problem5(int n) {  //O(2^n) It finds the n-th element of Fibonacci sequence
         if (n == 0) {
             return 0;
         } else if (n == 1) {
@@ -75,21 +76,21 @@ public class HelloApplication {
             return problem5(n - 1) + problem5(n - 2);
         }
     }
-    public static int problem6(int a, int n) {
+    public static int problem6(int a, int n) {  //O(n)   it takes 2 integers as an input. And multiplies first int with the degree of second int
         if (n == 0) {
             return 1;
         } else {
             return a * problem6(a, n - 1);
         }
     }
-    public static void problem7(int i, int[] arr) {
+    public static void problem7(int i, int[] arr) {  //O(n) it reverses an array of length n
         if (i < 0) {
             return;
         }
         System.out.print(arr[i] + " ");
         problem7(i - 1, arr);
     }
-    public static boolean problem8(String s) {
+    public static boolean problem8(String s) {   //O(n)  it checks if the inputted string is consisted by only digits
         if (s.isEmpty()) {
             return true;
         }
@@ -98,13 +99,13 @@ public class HelloApplication {
         }
         return problem8(s.substring(1));
     }
-    public static int problem9(int n, int k) {
+    public static int problem9(int n, int k) {   //O(2^n)  it finds binomial coefficient, considering first input as n and second input as k
         if (k == 0 || k == n) {
             return 1;
         }
         return problem9(n - 1, k - 1) + problem9(n - 1, k);
     }
-    public static int problem10(int a, int b) {
+    public static int problem10(int a, int b) { //O(log min(a,b)) it finds greatest common divisor of the two input digits using Euclidean Algorithm
         if (b == 0) {
             return a;
         }
